bf54f2b22d8942ce2e48f7b0119d93549d280d87
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-10-30 17:23:19 +0200

    fix the error

--- routes/v1.js
+++ routes/v1.js
@@ -1,57 +1,71 @@
 'use strict';
-const express = require('express');
-const Catigory = require('../lib/models/categories/categories-collection');
-const router = express.Router();
-const categoriesModle = new Catigory;
 
-router.post('/categories', (req, res, next) => {
-  categoriesModle.create(req.body).then((data) => {
+const express = require('express');
+const modelFinder = require('../lib/middleware/modelfinder');
+const router = express.Router();
+router.param('model', modelFinder);
+
+
+router.post('/:model', posting);
+router.get('/:model', getAll);
+router.get('/:model/:id', getById);
+router.put('/:model/:id', updating);
+router.patch('/:model/:id', patching);
+router.delete('/:model/:id', deleting);
+
+
+function posting(req, res, next) {
+    
+  req.model.create(req.body).then((data) => {
     let count = data.length;
     let results = data;
     res.status(200).json({ count, results });
   }).catch(next);
-});
+}
 
-router.get('/categories', (req, res, next) => {
-  categoriesModle.get().then((data) => {
+function getAll(req, res, next) {
+  req.model.get().then((data) => {
     let count = data.length;
     let results = data;
     res.status(200).json({ count, results });
   })
     .catch(next); /////////////////////////////
-});
+}
 
-router.get('/categories/:id', (req, res, next) => {
-  categoriesModle.get(req.params.id).then((data) => {
+function getById(req, res, next) {
+  req.model.get(req.params.id).then((data) => {
     let count = data.length;
     let results = data;
     res.status(200).json({ count, results });
   })
     .catch(next); /////////////////////////////
-});
+}
 
-router.put('/categories/:id', (req, res, next) => {
-  categoriesModle.update(req.params.id, req.body).then((data) => {
+function updating(req, res, next) {
+  req.model.update(req.params.id, req.body).then((data) => {
     let count = data.length;
     let results = data;
     res.status(200).json({ count, results });
   })
     .catch(next);
-});
+}
 
-router.patch('/categories/:id', (req, res, next) => {
-  categoriesModle.update(req.params.id, req.body).then((data) => {
+function patching(req, res, next) {
+  req.model.update(req.params.id, req.body).then((data) => {
     let count = data.length;
     let results = data;
     res.status(200).json({ count, results });
   })
     .catch(next);
-});
-router.delete('/categories/:id', (req, res, next) => {
-  categoriesModle.delete(req.params.id).then((data) => {
+}
+
+
+function deleting(req, res, next) {
+  req.model.delete(req.params.id).then((data) => {
     let results = data;
     res.status(200).json({ results });
   })
     .catch(next);
-});
+}
+
 module.exports = router;
